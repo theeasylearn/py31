a0b8d48c79bd72166da5774ffb2a081bd9eafca0
theeasylearn <theeasylearn@gmail.com>
2025-09-15 18:21:19 +0530

    first commit

---
+++ 6_area_calculator.py
@@ -0,0 +1,9 @@
+# write a program to accept length and width from user and calculate and display area
+length = input("Enter length")
+length = int(length)
+
+width = input("Enter width")
+width = int(width)
+
+area = length * width 
+print(f"area = {area}")
