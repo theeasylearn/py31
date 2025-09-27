74898643d851a06728441fbd7d6d68ebf0a04966
Drashti-Mehta-13 <drashtimehta08@gmail.com>
2025-09-27 05:30:59 -0700

    Add files via upload

---
+++ 1_arithmetic_operator.py
@@ -0,0 +1,27 @@
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+
+addition=num1+num2
+print(addition)
+
+subtraction=num1-num2
+print(subtraction)
+
+multiplication=num1*num2
+print(multiplication)
+
+division=num1/num2
+print(division)
+
+modulus=num1%num2
+print(modulus)
+#o/p:0
+
+exponent=num1**2
+print(exponent)
+#o/p:1024
+
+floordiv=num1//num2
+print(floordiv)
+#o/p:2
+
---
+++ 2_comparision_operator.py
@@ -0,0 +1,20 @@
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+
+result=num1==num2
+print(result)
+
+res1=num1!=num2
+print(res1)
+
+res2=num1>num2
+print(res2)
+
+res3=num1<num2
+print(res3)
+
+res4=num2>=num1
+print(res4)
+
+res5=num2<=num1
+print(res5)
