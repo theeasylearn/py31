97a5cf4b406c36a40d3ebf9c1e3998f963b735d4
theeasylearn <theeasylearn@gmail.com>
2025-10-30 19:46:20 +0530

    38 split

---
+++ 38_split.py
@@ -0,0 +1,9 @@
+teams = "China,France,Japan,Spain,Brazil,Canada,India,Russia,Germany,Italy"
+print(teams) 
+#convert list using split 
+list = teams.split(",")
+print(list[0]) #China
+print(list[1]) #France
+print(list[2]) #Japan
+print(list[3]) #Spain
+print(list)
